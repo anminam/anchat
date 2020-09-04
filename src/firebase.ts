be584bc3a418d6322a82dac72bdbae53e0644d1d
minamAn <minamhunmin@gmail.com>
2020-09-05 08:32:27 +0900

    add firebase

---
+++ src/firebase.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBdErZkMBmlDQnL7T8S95qQ61KImmrFuY4",
+  authDomain: "anchat-d51bb.firebaseapp.com",
+  databaseURL: "https://anchat-d51bb.firebaseio.com",
+  projectId: "anchat-d51bb",
+  storageBucket: "anchat-d51bb.appspot.com",
+  messagingSenderId: "352402568314",
+  appId: "1:352402568314:web:72eb4ded5e07b0265f37c3",
+  measurementId: "G-GFQ65E30GK",
+};
